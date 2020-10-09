5f4785c005e865f236ada0d070171f9bc078392c
wakeupmh <wakeupmh@gmail.com>
2020-10-09 10:00:47 -0300

    test(response): add response tests

--- test/setup.js
+++ test/setup.js
@@ -3,8 +3,11 @@ import sinon from 'sinon'
 import dotenv from 'dotenv'
 import 'module-alias/register'
 import chaiAsPromised from 'chai-as-promised'
+
 chai.use(chaiAsPromised)
 chai.config.includeStack = true
+
 dotenv.config()
+
 global.expect = expect
 global.sinon = sinon
